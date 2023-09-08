bc5fc9003e4d3bedd2d6d83a5052858f09fb19f8
asbdu08122620 <144431380+asbdu08122620@users.noreply.github.com>
2023-09-08 14:04:36 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+#leaf year" " " "
+
+#defining a function
+def isleapyear(year):
+  if(year%4==0 and year%100!=0):
+    return True 
+  else:
+    return False 
+year=int(input("enter a year"))
+if isleapyear(year):
+  print('{}is a laepyear.'.format(year))
+else:
+  print('{} is not a leapyear.'.format(year))
